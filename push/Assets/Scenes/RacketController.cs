132f412ef0ad7403e97dd5cb1da24f4ddc7d4ac7
大羽由華 <obayuka@oobayukanoMacBook-Pro.local>
2019-01-22 17:41:17 +0900

    first commit

---
+++ push/Assets/Scenes/RacketController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (-0.1f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate ( 0.1f, 0, 0);
+        }   
+        if (Input.GetKeyDown (KeyCode.Space)){
+            transform.Rotate (new Vector3(30f, 0f, 0f));
+        }
+        if (Input.GetKeyUp (KeyCode.Space)){
+            transform.Rotate (new Vector3(-30f, 0f, 0f));
+        }
+    }
+
+    void OnCollisionEnter(Collision coll) {
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 10000000000);
+
+    }
+}
---
+++ shoot/Assets/SampleShooting/rock_controll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock_controll : MonoBehaviour
+{
+
+    float fallSpeed;
+	float rotSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+     	this.fallSpeed = 0.01f + 0.1f * Random.value;
+		this.rotSpeed = 5f + 3f * Random.value;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate( 0, -fallSpeed, 0, Space.World);
+		transform.Rotate(0, 0, rotSpeed );
+
+		if (transform.position.y < -5.5f) {
+            GameObject.Find ("Canvas").GetComponent<UIcontroller> ().GameOver ();
+			Destroy (gameObject);
+		}
+    }
+}
